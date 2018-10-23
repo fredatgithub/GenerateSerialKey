b1cc585cb812513a8b5fa500bd6deca1f6cefa18
fred <domaine75@gmail.com>
2018-10-23 18:06:04 +0200

    Add unit test

--- GenerateSerialKey/FormMain.Designer.cs
+++ GenerateSerialKey/FormMain.Designer.cs
@@ -31,14 +31,17 @@
       this.buttonGenerate = new System.Windows.Forms.Button();
       this.textBoxKey = new System.Windows.Forms.TextBox();
       this.buttonVerifyKey = new System.Windows.Forms.Button();
-      this.textBoxCheckKey = new System.Windows.Forms.TextBox();
+      this.textBoxCheckKeyValidity = new System.Windows.Forms.TextBox();
+      this.textBoxKeyGenerationDate = new System.Windows.Forms.TextBox();
+      this.textBoxKeyExpiration = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
       // 
       // buttonGenerate
       // 
-      this.buttonGenerate.Location = new System.Drawing.Point(55, 51);
+      this.buttonGenerate.Location = new System.Drawing.Point(82, 78);
+      this.buttonGenerate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.buttonGenerate.Name = "buttonGenerate";
-      this.buttonGenerate.Size = new System.Drawing.Size(101, 35);
+      this.buttonGenerate.Size = new System.Drawing.Size(152, 54);
       this.buttonGenerate.TabIndex = 0;
       this.buttonGenerate.Text = "Generate";
       this.buttonGenerate.UseVisualStyleBackColor = true;
@@ -46,38 +49,64 @@
       // 
       // textBoxKey
       // 
-      this.textBoxKey.Location = new System.Drawing.Point(181, 65);
+      this.textBoxKey.Location = new System.Drawing.Point(272, 100);
+      this.textBoxKey.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.textBoxKey.Name = "textBoxKey";
-      this.textBoxKey.Size = new System.Drawing.Size(384, 20);
+      this.textBoxKey.Size = new System.Drawing.Size(348, 26);
       this.textBoxKey.TabIndex = 1;
       // 
       // buttonVerifyKey
       // 
-      this.buttonVerifyKey.Location = new System.Drawing.Point(55, 151);
+      this.buttonVerifyKey.Location = new System.Drawing.Point(82, 232);
+      this.buttonVerifyKey.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
       this.buttonVerifyKey.Name = "buttonVerifyKey";
-      this.buttonVerifyKey.Size = new System.Drawing.Size(101, 35);
+      this.buttonVerifyKey.Size = new System.Drawing.Size(152, 54);
       this.buttonVerifyKey.TabIndex = 2;
       this.buttonVerifyKey.Text = "Check Key";
       this.buttonVerifyKey.UseVisualStyleBackColor = true;
+      this.buttonVerifyKey.Click += new System.EventHandler(this.ButtonVerifyKey_Click);
       // 
-      // textBoxCheckKey
+      // textBoxCheckKeyValidity
       // 
-      this.textBoxCheckKey.Location = new System.Drawing.Point(181, 159);
-      this.textBoxCheckKey.Name = "textBoxCheckKey";
-      this.textBoxCheckKey.Size = new System.Drawing.Size(384, 20);
-      this.textBoxCheckKey.TabIndex = 3;
+      this.textBoxCheckKeyValidity.Location = new System.Drawing.Point(272, 245);
+      this.textBoxCheckKeyValidity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.textBoxCheckKeyValidity.Name = "textBoxCheckKeyValidity";
+      this.textBoxCheckKeyValidity.Size = new System.Drawing.Size(348, 26);
+      this.textBoxCheckKeyValidity.TabIndex = 3;
+      this.textBoxCheckKeyValidity.Text = "Key is valid or not";
       // 
-      // Form1
+      // textBoxKeyGenerationDate
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.textBoxKeyGenerationDate.Location = new System.Drawing.Point(272, 302);
+      this.textBoxKeyGenerationDate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.textBoxKeyGenerationDate.Name = "textBoxKeyGenerationDate";
+      this.textBoxKeyGenerationDate.Size = new System.Drawing.Size(348, 26);
+      this.textBoxKeyGenerationDate.TabIndex = 4;
+      this.textBoxKeyGenerationDate.Text = "Key was generated on ";
+      // 
+      // textBoxKeyExpiration
+      // 
+      this.textBoxKeyExpiration.Location = new System.Drawing.Point(272, 356);
+      this.textBoxKeyExpiration.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.textBoxKeyExpiration.Name = "textBoxKeyExpiration";
+      this.textBoxKeyExpiration.Size = new System.Drawing.Size(348, 26);
+      this.textBoxKeyExpiration.TabIndex = 5;
+      this.textBoxKeyExpiration.Text = "Key will expire on";
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 246);
-      this.Controls.Add(this.textBoxCheckKey);
+      this.ClientSize = new System.Drawing.Size(814, 426);
+      this.Controls.Add(this.textBoxKeyExpiration);
+      this.Controls.Add(this.textBoxKeyGenerationDate);
+      this.Controls.Add(this.textBoxCheckKeyValidity);
       this.Controls.Add(this.buttonVerifyKey);
       this.Controls.Add(this.textBoxKey);
       this.Controls.Add(this.buttonGenerate);
-      this.Name = "Form1";
-      this.Text = "Form1";
+      this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.Name = "FormMain";
+      this.Text = "Generate key valid one month";
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -88,7 +117,9 @@
     private System.Windows.Forms.Button buttonGenerate;
     private System.Windows.Forms.TextBox textBoxKey;
     private System.Windows.Forms.Button buttonVerifyKey;
-    private System.Windows.Forms.TextBox textBoxCheckKey;
+    private System.Windows.Forms.TextBox textBoxCheckKeyValidity;
+    private System.Windows.Forms.TextBox textBoxKeyGenerationDate;
+    private System.Windows.Forms.TextBox textBoxKeyExpiration;
   }
 }
 
--- GenerateSerialKey/FormMain.cs
+++ GenerateSerialKey/FormMain.cs
@@ -19,9 +19,50 @@ namespace GenerateSerialKey
     private static string CreateNewKey()
     {
       string result = string.Empty;
-      
       //AAAAA-AADDAA-AMMAAA-AAYYYYAA
+      for (int i = 0; i < 6; i++)
+      {
+        result += GenerateRandomNumberUsingCrypto(0, 9).ToString();
+      }
+
+      result += "-";
+      for (int i = 0; i < 2; i++)
+      {
+        result += GenerateRandomNumberUsingCrypto(0, 9).ToString();
+      }
+
+      result += AddZero(DateTime.Now.Day);
+      for (int i = 0; i < 2; i++)
+      {
+        result += GenerateRandomNumberUsingCrypto(0, 9).ToString();
+      }
+
+      result += "-";
+      //AAAAA-AADDAA-AMMAAA-AAYYYYAA
+      result += GenerateRandomNumberUsingCrypto(0, 9).ToString();
+      result += AddZero(DateTime.Now.Month);
+      for (int i = 0; i < 3; i++)
+      {
+        result += GenerateRandomNumberUsingCrypto(0, 9).ToString();
+      }
+
+      result += "-";
+      for (int i = 0; i < 2; i++)
+      {
+        result += GenerateRandomNumberUsingCrypto(0, 9).ToString();
+      }
+
+      result += DateTime.Now.Year;
+      for (int i = 0; i < 2; i++)
+      {
+        result += GenerateRandomNumberUsingCrypto(0, 9).ToString();
+      }
       return result;
+    }
+
+    private static string AddZero(int number)
+    {
+      return number < 10 ? $"0{number}" : number.ToString();
     }
 
     public static string GenerateRandomString(int numberOfChar = 1)
@@ -41,6 +82,7 @@ namespace GenerateSerialKey
       {
         return 0;
       }
+
       int result;
       var crypto = new RNGCryptoServiceProvider();
       var randomNumber = new byte[1];
@@ -53,5 +95,49 @@ namespace GenerateSerialKey
       return result;
     }
 
+    private void ButtonVerifyKey_Click(object sender, EventArgs e)
+    {
+      if (textBoxKey.Text.Trim() == string.Empty)
+      {
+        return;
+      }
+
+      //AAAAA-AADDAA-AMMAAA-AAYYYYAA
+      //1234567890123456789012345678
+      //         10        20
+      string theKey = textBoxKey.Text;
+      bool keyIsValid = true;
+      DateTime keyGenerationDate;
+      DateTime keyExpirationDate;
+      int extractedDay;
+      if (!int.TryParse(theKey.Substring(9, 2), out extractedDay))
+      {
+        keyIsValid = false;
+      }
+
+      int extractedMonth;
+      if (!int.TryParse(theKey.Substring(15, 2), out extractedMonth))
+      {
+        keyIsValid = false;
+      }
+
+      int extractedYear;
+      if (!int.TryParse(theKey.Substring(23, 4), out extractedYear))
+      {
+        keyIsValid = false;
+      }
+
+      textBoxCheckKeyValidity.Text = $"Key is {Negate(keyIsValid)}valid";
+
+      DateTime extractedDate = new DateTime(extractedYear, extractedMonth, extractedDay);
+
+      textBoxKeyGenerationDate.Text = $"Key was generated on {extractedDate.ToShortDateString()}";
+      textBoxKeyExpiration.Text = $"Key will expired on {extractedDate.AddDays(30).ToShortDateString()}";
+    }
+
+    private static string Negate(bool trueOrFalse)
+    {
+      return trueOrFalse ? string.Empty : "not ";
+    }
   }
 }
